6c19d582cde2738bdb067081fcdf85febf59b1dd
Kamis83 <bajerka@gmail.com>
2022-05-28 09:33:41 +0200

    First Commit

---
+++ GamesTest.java
@@ -0,0 +1,57 @@
+package com.Kamis83;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GamesTest {
+
+    @Test
+    void countLengthOfSentence() {
+
+        //given
+        String sentence = "Ania chodzi do domu";
+        int length;
+
+        // when
+        length = sentence.length();
+
+        //then
+        assertEquals(19,length);
+    }
+
+    @Test
+    void countNumberOfWords() {
+        //given
+        String sentence = "Ania chodzi do domu";
+        int numberOfWords;
+
+        // when
+        String[] words = sentence.split("\\s+");
+                numberOfWords = words.length;
+
+        //then
+        assertEquals(4,numberOfWords);
+    }
+
+    @Test
+    void showSeparateLetters() {
+
+        //given
+        String sentence = "Ania ";
+        char[]letters;
+
+
+        // when
+        letters = sentence.toCharArray();
+        for (int i = 0; i < sentence.length() ; i++) {
+            System.out.println( letters[i]);
+        }
+
+
+        //then
+        assertEquals('A',letters[0]);
+    }
+}
